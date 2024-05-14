4653b8ad4c2ebd2749b2bcb52799d99e292e3051
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-15 06:03:47 +0700

    footer

--- new-app/src/pages/Dashboard/Footer/index.tsx
+++ new-app/src/pages/Dashboard/Footer/index.tsx
@@ -1,3 +1,10 @@
 export default function FooterPage() {
-  return <p>Copy Right 2024</p>;
+  return (
+    <p>
+      {" "}
+      &copy;pokemon. &copy;Nitendo/Creatures Inc./GAME FREAK inc.
+      &copy;Nitendo,Creatures, GAME FREAK,TV Tokyo, ShopPro, JR Kikaku.
+      &copy;Pokemon.TM and &#174; are trademarks of Nitendo{" "}
+    </p>
+  );
 }
