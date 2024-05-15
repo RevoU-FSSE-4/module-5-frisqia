612d967b1f0a916d6fee96259104cb41b0afcfd6
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-15 21:10:45 +0700

    json test

--- new-app/src/pages/Dashboard/Form/LoginForm.tsx
+++ new-app/src/pages/Dashboard/Form/LoginForm.tsx
@@ -1,17 +1,144 @@
 import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 interface RegisterUser {
   email: string;
   password: string;
 }
 
-export default function LoginForm() {
+interface RegisterUser {
+  email: string;
+  password: string;
+}
+
+function LoginForm() {
+  // Menggunakan useNavigate untuk navigasi
+  const loginValid = Yup.object({
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .matches(/[0-9]/, "Password must contain at least one number")
+      .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    validationSchema: loginValid,
+    onSubmit: (values) => {
+      handleLogin(values);
+    },
+  });
+  async function handleLogin(credential: RegisterUser) {
+    try {
+      const body = {
+        email: credential.email,
+        password: credential.password,
+      };
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      };
+      const response = await fetch(
+        "https://library-crud-sample.vercel.app/api/user/login",
+        options
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(
+          "Failed to login, Maybe your email or password not matched"
+        );
+      } else {
+        localStorage.setItem("token", data.token);
+        console.log(data);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  // Fungsi untuk navigasi ke halaman RegisterForm
   const router = useRouter();
 
   return (
-    <form>
-      <p>Haven't account?</p>
-      <button onClick={() => router.push("/RegsterForm")}>Registrer</button>
+    <form
+      onSubmit={formik.handleSubmit}
+      className="max-w-sm mx-auto mt-8 bg-orange-100 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
+      <label
+        htmlFor="email"
+        className="block text-gray-700 text-sm font-bold mb-2"
+      >
+        Email
+      </label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div className="text-red-500">{formik.errors.email}</div>
+      ) : null}
+      <label
+        htmlFor="password"
+        className="block text-gray-700 text-sm font-bold mt-4 mb-2"
+      >
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div className="text-red-500">{formik.errors.password}</div>
+      ) : null}
+      <br />
+      <div>
+        <br />
+        <div className="flex justify-between items-center">
+          <button
+            type="submit"
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Sign In
+          </button>
+          {/* Tombol untuk menuju halaman RegisterForm */}
+          <button
+            type="button"
+            onClick={() => router.push("/RegsterForm")} // Memanggil fungsi handleRegisterClick saat tombol diklik
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Register
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
+export default LoginForm;
+
+// export default function LoginForm() {
+//   const router = useRouter();
+
+//   return (
+//     <form>
+//       <p>Haven't account?</p>
+//       <button onClick={() => router.push("/RegsterForm")}>Registrer</button>
+//     </form>
+//   );
+// }
