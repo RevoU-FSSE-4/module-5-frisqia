bcfe51c12316ad16539b61e614b3275cde7234ad
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-18 22:03:50 +0700

    pokemon find

--- new-app/src/pages/Dashboard/Pokemon/[pokemonId].tsx
+++ new-app/src/pages/Dashboard/Pokemon/[pokemonId].tsx
@@ -1,42 +1,87 @@
-import { GetServerSideProps } from "next";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 export const FindPokemon = () => {
   const router = useRouter();
   const [query, setQuery] = useState<string>("");
-  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setQuery(value);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
   };
+
   const handleClick = () => {
-    router.push(`/Dashboard/Pokemon/${query.toLowerCase()}`);
+    if (query.trim() !== "") {
+      router.push(`/Dashboard/Pokemon/${query.toLowerCase()}`);
+    }
   };
+
   return (
-    <div>
+    <div className="flex justify-center mt-4">
       <input
         type="text"
         value={query}
-        onChange={handelChange}
+        onChange={handleChange}
         placeholder="Find your pokemon"
+        className="border-2 border-orange-500 rounded-l-lg p-2 w-1/3"
       />
-      <button type="button" onClick={handleClick}>
-        search
+      <button
+        type="button"
+        onClick={handleClick}
+        className="bg-orange-500 text-white rounded-r-lg p-2 hover:bg-orange-700"
+      >
+        Search
       </button>
     </div>
   );
 };
 
+export default function PokemonIdPage({
+  pokemon,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <Head>
+        <title>Pokemon: {pokemon.name}</title>
+        <meta property="pokemon-name" content={pokemon.name} key="title" />
+      </Head>
+      <div className="flex flex-col items-center bg-orange-100 p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">
+          Pokemon ID {pokemon.id}: {pokemon.name}
+        </h1>
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="w-48 h-48 mb-4"
+        />
+        <p className="text-xl mb-2">Height: {pokemon.height}</p>
+        <p className="text-xl mb-4">Weight: {pokemon.weight}</p>
+        <h3 className="text-2xl font-semibold mb-2">Abilities:</h3>
+        <ul className="list-disc list-inside">
+          {pokemon.abilities.map((abilityInfo: any) => (
+            <li key={abilityInfo.ability.name} className="text-lg">
+              {abilityInfo.ability.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { pokemonId } = context.params as { pokemonId: string };
   const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit/${pokemonId} `
+    `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
   );
+
   if (!response.ok) {
     return {
       notFound: true,
     };
   }
+
   const pokemon = await response.json();
   return { props: { pokemon } };
 };
--- new-app/src/pages/Dashboard/Pokemon/index.tsx
+++ new-app/src/pages/Dashboard/Pokemon/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { FindPokemon } from "./[pokemonId]";
@@ -25,7 +25,7 @@ interface PokemonResponse {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10"); // Mengambil 10 Pokemon pertama
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20"); // Mengambil 20 Pokemon pertama
   const data: PokemonResponse = await response.json();
 
   // Mengambil data detail untuk setiap Pokemon
@@ -41,42 +41,75 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 export default function PokemonPage({ pokemons }: { pokemons: Pokemon[] }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pokemonsPerPage = 9;
+  const totalPages = Math.ceil(pokemons.length / pokemonsPerPage);
+
+  const startIndex = (currentPage - 1) * pokemonsPerPage;
+  const endIndex = Math.min(startIndex + pokemonsPerPage, pokemons.length);
+  const currentPokemonSubset = pokemons.slice(startIndex, endIndex);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
   return (
     <>
       <Head>
         <title>Daftar Pokemon</title>
       </Head>
-      <div>
+      <div className="text-center">
         <FindPokemon />
 
-        {pokemons.map((pokemon) => (
-          <div
-            key={pokemon.name}
-            // style={{
-            //   border: "1px solid #ccc",
-            //   margin: "10px",
-            //   padding: "10px",
-            // }}
+        <div className="grid grid-cols-3 gap-6 p-6">
+          {currentPokemonSubset.map((pokemon) => (
+            <div
+              key={pokemon.name}
+              className="bg-orange-500 rounded-lg p-4 text-white"
+            >
+              <h1 className="text-xl font-bold">{pokemon.name}</h1>
+              <img
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                className="mx-auto my-4"
+                width={96}
+                height={96}
+              />
+              {/* Uncomment these lines if you want to display more details */}
+              {/* <p>Tinggi: {pokemon.height}</p>
+              <p>Berat: {pokemon.weight}</p>
+              <h3>Kemampuan:</h3>
+              <ul>
+                {pokemon.abilities.map((abilityInfo) => (
+                  <li key={abilityInfo.ability.name}>
+                    {abilityInfo.ability.name}
+                  </li>
+                ))}
+              </ul> */}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-center mt-4 space-x-4">
+          <button
+            onClick={prevPage}
+            disabled={currentPage === 1} // Tombol sebelumnya dinonaktifkan jika halaman slide saat ini adalah halaman pertama
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-l focus:outline-none focus:shadow-outline"
           >
-            <h1>{pokemon.name}</h1>
-            <img
-              src={pokemon.sprites.front_default}
-              alt={pokemon.name}
-              width={96}
-              height={96}
-            />
-            <p>Tinggi: {pokemon.height}</p>
-            <p>Berat: {pokemon.weight}</p>
-            <h3>Kemampuan:</h3>
-            <ul>
-              {pokemon.abilities.map((abilityInfo) => (
-                <li key={abilityInfo.ability.name}>
-                  {abilityInfo.ability.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+            Prev
+          </button>
+          <button
+            onClick={nextPage}
+            disabled={currentPage === totalPages} // Tombol berikutnya dinonaktifkan jika halaman slide saat ini adalah halaman terakhir
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-r focus:outline-none focus:shadow-outline"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </>
   );
